fc41320dd2dcabff89524fd5888fda9ed87e8e0e
Aime Malaika <aimemalaika1995@gmail.com>
2022-04-01 15:01:27 +0200

    Fixed linters errors

--- app.rb
+++ app.rb
@@ -1,26 +1,29 @@
 def decode_morse_code()
-    morsecode = [  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." ] 
-    morsevalues = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    puts "Enter a word to translate to morse code \n e.g: .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...";
-    code = gets.chomp;
-    code_array = code.split("  ");
-    string = "";
-    code_array.each do |word|
-        word_array = word.split(" ");
-        word_array.each do |letter|
-            string += morsevalues[morsecode.index(letter)];
-        end
-        string += " ";
+  morsecode = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---',
+               '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+  morsevalues = %w[a b c d e f g h i j k l m n o p q r s t
+                   u v w x y z]
+  puts "Enter a word to translate to morse code \n e.g: .-   -... --- -..-   ..-. ..- .-.. .-..
+--- ..-.   .-. ..- -... .. . ..."
+  code = gets.chomp
+  code_array = code.split('  ')
+  string = ''
+  code_array.each do |word|
+    word_array = word.split
+    word_array.each do |letter|
+      string += morsevalues[morsecode.index(letter)]
     end
-    return "The translated code is:\n ====> #{string}";
+    string += ' '
+  end
+  "The translated code is:\n ====> #{string}"
 end
 
-iHaveAMessage = true;
+i_have_a_message = true
 
-while iHaveAMessage
-    puts decode_morse_code();
-    puts "Would you like to translate another message? (y/n)";
-    iHaveAMessage = gets.chomp == "y";
+while i_have_a_message
+  puts decode_morse_code
+  puts 'Would you like to translate another message? (y/n)'
+  i_have_a_message = gets.chomp == 'y'
 end
 
-puts "Goodbye!";
+puts 'Goodbye!'
